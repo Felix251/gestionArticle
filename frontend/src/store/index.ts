2490f7682c3f8b031c4cc004a2bc846dae46fa4a
Vadx <lv_km@ukr.net>
2022-11-14 20:34:24 +0200

    Add Redux Provider

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { postAPI } from './api/postAPI';
+ 
+
+export const store = configureStore({
+  reducer: {
+    // auth: authReducer
+    [postAPI.reducerPath]: postAPI.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({}).concat([postAPI.middleware]),
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
