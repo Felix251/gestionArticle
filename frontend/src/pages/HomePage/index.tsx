b7b55fce69d6b6a09bdf79bfec99721147872386
Vadx <lv_km@ukr.net>
2022-11-15 22:12:36 +0200

    UPD home page

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,12 +1,49 @@
-import { Typography } from 'antd'
+import React from 'react';
+import { Card, Divider, Typography } from 'antd'
 const { Title } = Typography
- 
-const HomePage = () => {
+
+const gridStyle: React.CSSProperties = {
+  width: '25%',
+  textAlign: 'center',
+  fontWeight: 'bold',
+  color: '#333'
+};
+
+const HomePage: React.FC = () => {
   return (
-    <div className='alignCenter pt50'>
-      <Title>React Template using:</Title>
-      <Title level={2} type='secondary'>TypeScript, Ant Design, React Toolkit, RTK Query</Title>
-    </div>
+    <>
+      <div className='alignCenter pt50'>
+        <Title>React Template using:</Title>
+        <Title level={2} type='secondary'>TypeScript, Ant Design, React Toolkit, RTK Query</Title>
+      </div>
+      <Divider />
+      <Card title="🎬 Feachures" style={{ width: '100%', marginBottom: 30 }}>
+        <Card.Grid style={gridStyle}>React V18.2</Card.Grid>
+        <Card.Grid style={gridStyle}>TypeScript</Card.Grid>
+        <Card.Grid style={gridStyle}>Ant Design</Card.Grid>
+        <Card.Grid style={gridStyle}>React Toolkit</Card.Grid>
+        <Card.Grid style={gridStyle}>ReactRouterDom V6.4</Card.Grid>
+        <Card.Grid style={gridStyle}>SEO support</Card.Grid>
+        <Card.Grid style={gridStyle}>Animation</Card.Grid>
+        <Card.Grid style={gridStyle}>RTK Query</Card.Grid>
+      </Card>
+      
+      <Card
+        title='🥎  Packages' bordered={false} style={{ width: '100%', marginBottom: 30 }}
+      >
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+
+      <Card
+        title='⛳ Pages' bordered={false} style={{ width: '100%', marginBottom: 30 }}
+      >
+        <p>Card content</p>
+        <p>Card content</p>
+        <p>Card content</p>
+      </Card>
+    </>
   )
 }
 
